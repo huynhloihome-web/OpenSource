06e3366a6bddf0678ae507619d6e04e569c85441
quynhanh1909 <030239230012@st.buh.edu.vn>
2026-03-26 09:38:05 +0700

    bài 12

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SachController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sach', [SachController::class, 'index'])->name('sach.index');
+Route::get('/sach/theloai/{id}', [SachController::class, 'theoTheLoai'])->name('sach.theloai');
+Route::get('/sach/{id}', [SachController::class, 'show'])->name('show');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
